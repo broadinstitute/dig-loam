ae015fcfacba6ae917271e16c4b8daf77ad2ec09
ryank <ryank@dig-ae-dev-01.broadinstitute.org>
2019-02-11 15:51:09 -0500

    restructuring commit before tagging v2.1

--- src/archive/data_processing_v2.1.1/scripts/meta_exclusions_table.r
+++
@@ -1,9 +0,0 @@
-library(argparse)
-
-parser <- ArgumentParser()
-parser$add_argument("--meta-exclusions", dest="meta_exclusions", type="character", help="a comma separated list of metas, cohorts, and exclusions files each separated by 3 underscores")
-parser$add_argument("--meta-cohorts", dest="meta_cohorts", type="character", help="a comma separated list of meta and + separated cohorts each separated by 3 underscores")
-parser$add_argument("--out", dest="out", type="character", help="an output filename")
-args<-parser$parse_args()
-
-print(args)
