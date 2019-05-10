c60a3ce07c10577d92d8617dcf7eaac161cd41b4
ryank <ryank@dig-ae-dev-01.broadinstitute.org>
2019-05-10 16:57:18 -0400

    added plink_to_vcf_no_half_calls.sh to convert plink file to vcf file with the intermediate step of converting haploid calls to diploid calls

---
+++ src/scripts/plink_to_vcf_no_half_calls.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+plink=$1
+base=$2
+forceA2=$3
+refBase=$4
+mem=$5
+vcf=$6
+
+$plink --bfile $base --allow-no-sex --output-chr MT --recode vcf-iid bgz --real-ref-alleles --a2-allele $forceA2 --out $refBase --memory $mem
+$plink --vcf $vcf --allow-no-sex --output-chr MT --vcf-half-call haploid --recode vcf-iid bgz --real-ref-alleles --a2-allele $forceA2 --out $refBase --memory $mem
