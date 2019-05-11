44e0431828dc6abcebe6334057f49a3fab71db70
ryank <ryank@dig-ae-dev-01.broadinstitute.org>
2019-05-11 16:12:35 -0400

    removed call_rate_res and n_called_res from istats_adj_pca.r

--- src/scripts/istats_adj_pca.r
+++ src/scripts/istats_adj_pca.r
@@ -14,10 +14,13 @@ print(args)
 
 x<-read.table(args$sampleqc_stats_adj,header=T,as.is=T,stringsAsFactors=F)
 
+# note: the residuals of call rate based metrics are not normal, so a hard callrate threshold is now used
 # set samples whose values are high on unidirectional metrics to the mean of the metric across all samples
-for(m in c("call_rate_res","n_called_res")) {
-	x[,m][x[,m] > mean(x[,m])] <- mean(x[,m][x[,m] > mean(x[,m])])
-}
+#for(m in c("call_rate_res","n_called_res")) {
+#	x[,m][x[,m] > mean(x[,m])] <- mean(x[,m][x[,m] > mean(x[,m])])
+#}
+x$call_rate_res <- NULL
+x$n_called_res <- NULL
 
 row.names(x)<-x$IID
 trans = preProcess(x[,2:ncol(x)], method=c("BoxCox", "medianImpute", "center", "scale"),thresh=1.0)
