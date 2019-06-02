a295cdaceb13e8613ce3ea1fc82b21446f3979eb
ryank <ryank@dig-ae-dev-01.broadinstitute.org>
2019-06-02 06:06:18 -0400

    fixed bug in plot_ancestry_cluster.r caused by self report codes not matching 1kg super populations

--- src/scripts/plot_ancestry_cluster.r
+++ src/scripts/plot_ancestry_cluster.r
@@ -24,30 +24,34 @@ args<-parser$parse_args()
 
 print(args)
 
-dat<-read.table(args$pca_scores, header=T, as.is=T, stringsAsFactors=F)
-cl<-read.table(args$cluster, as.is=T, skip=1, stringsAsFactors=F)
-names(cl)[1]<-"CLUSTER"
-dat<-cbind(dat,cl)
-pheno<-read.table(args$sample_file, header=T, as.is=T, stringsAsFactors=F, sep="\t")
-pheno<-pheno[,c(args$sample_id,args$sr_race)]
-pheno[,args$sr_race]<-paste0("sr_",pheno[,args$sr_race])
-names(pheno)[1]<-"IID"
-dat<-merge(dat,pheno,all.x=T)
-
-outfile<-paste(unlist(strsplit(args$pca_scores,"/"))[1:(length(unlist(strsplit(args$pca_scores,"/")))-1)],args$project_id,sep="/")
 gg_color_hue <- function(n) {
   hues = seq(15, 375, length=n+1)
   hcl(h=hues, l=65, c=100)[1:n]
 }
-color<-gg_color_hue(max(dat$CLUSTER))
 
+pcs<-read.table(args$pca_scores, header=T, as.is=T, stringsAsFactors=F)
+cl<-read.table(args$cluster, as.is=T, skip=1, stringsAsFactors=F)
+names(cl)[1]<-"CLUSTER"
+pcs<-cbind(pcs,cl)
+sample_file<-read.table(args$sample_file, header=T, as.is=T, stringsAsFactors=F, sep="\t")
+sample_file<-sample_file[,c(args$sample_id,args$sr_race)]
+sample_file[,args$sr_race]<-paste0("sr_",sample_file[,args$sr_race])
+names(sample_file)[1]<-"IID"
+pheno<-merge(pcs,sample_file,all.x=T)
+
+color<-gg_color_hue(max(pheno$CLUSTER))
+pheno$COLOR<-color[pheno$CLUSTER]
+print(head(pheno))
+
+cat("\nplotting clusters\n")
 pdf(args$cluster_plots,width=7, height=7)
-for(i in grep("^PC",names(dat))[-length(grep("^PC",names(dat)))]) {
-	p<-ggplot(dat, aes(dat[,i],dat[,i+1])) +
-		geom_point(aes(color=factor(CLUSTER),shape=factor(GROUP))) +
-		labs(x=paste("PC",i-grep("^PC",names(dat))[1]+1,sep=""),y=paste("PC",i-grep("^PC",names(dat))[1]+2,sep=""),shape="COHORT",colour="CLUSTER") +
+for(i in grep("^PC",names(pheno))[-length(grep("^PC",names(pheno)))]) {
+	p<-ggplot(pheno, aes(pheno[,i],pheno[,i+1])) +
+		geom_point(aes(color=factor(COLOR),shape=factor(GROUP))) +
+		labs(x=paste("PC",i-grep("^PC",names(pheno))[1]+1,sep=""),y=paste("PC",i-grep("^PC",names(pheno))[1]+2,sep=""),shape="COHORT",colour="CLUSTER") +
+		scale_color_manual(labels = sort(unique(pheno$CLUSTER)), values = color) +
 		theme_bw() +
-		guides(col = guide_legend(override.aes = list(shape = 15, size = 10))) +
+		guides(col = guide_legend(override.aes = list(shape = 15, size = 6))) +
 		theme(axis.line = element_line(colour = "black"), 
 		plot.title = element_blank(),
 		panel.grid.major = element_blank(),
@@ -59,127 +63,142 @@ for(i in grep("^PC",names(dat))[-length(grep("^PC",names(dat)))]) {
 }
 dev.off()
 
+cat("\ndetermining unknown clusters\n")
 clusters_unknown<-c()
 clusters_exclude<-c()
-for(i in unique(dat$CLUSTER)[unique(dat$CLUSTER) != 1]) {
-	if(nrow(dat[dat$CLUSTER == i & dat$GROUP == args$project_id,]) > 0) {
+for(i in unique(pheno$CLUSTER)[unique(pheno$CLUSTER) != 1]) {
+	if(nrow(pheno[pheno$CLUSTER == i & pheno$GROUP == args$project_id,]) > 0) {
 		clusters_unknown<-c(clusters_unknown,i)
 	} else {
 		clusters_exclude<-c(clusters_exclude,i)
 	}
 }
+print(sort(clusters_unknown))
 
+cat("\ndetermining centers of 1kg super populations\n")
 i<-0
-cohorts_1kg<-unique(dat$GROUP)[unique(dat$GROUP) != args$project_id]
+cohorts_1kg<-unique(pheno$GROUP)[unique(pheno$GROUP) != args$project_id]
 for(c in cohorts_1kg) {
 	i<-i+1
 	if(i == 1) {
-		centers_1kg<-colMeans(dat[,c("PC1","PC2","PC3")][dat$GROUP == c,])
+		centers_1kg<-colMeans(pheno[,c("PC1","PC2","PC3")][pheno$GROUP == c,])
 	} else {
-		centers_1kg<-rbind(centers_1kg,colMeans(dat[,c("PC1","PC2","PC3")][dat$GROUP == c,]))
+		centers_1kg<-rbind(centers_1kg,colMeans(pheno[,c("PC1","PC2","PC3")][pheno$GROUP == c,]))
 	}
 }
 row.names(centers_1kg)<-cohorts_1kg
 centers_1kg<-as.data.frame(centers_1kg)
+print(centers_1kg)
 
+cat("\ndetermining centers of project clusters\n")
 i<-0
 for(c in clusters_unknown) {
 	i<-i+1
 	if(i == 1) {
-		centers_unknown<-colMeans(dat[,c("PC1","PC2","PC3")][dat$CLUSTER == c & dat$GROUP == args$project_id,])
+		centers_project<-colMeans(pheno[,c("PC1","PC2","PC3")][pheno$CLUSTER == c & pheno$GROUP == args$project_id,])
 	} else {
-		centers_unknown<-rbind(centers_unknown,colMeans(dat[,c("PC1","PC2","PC3")][dat$CLUSTER == c & dat$GROUP == args$project_id,]))
+		centers_project<-rbind(centers_project,colMeans(pheno[,c("PC1","PC2","PC3")][pheno$CLUSTER == c & pheno$GROUP == args$project_id,]))
 	}
 }
 for(c in clusters_exclude) {
-	centers_unknown<-rbind(centers_unknown,data.frame(PC1=NA,PC2=NA,PC3=NA))
+	centers_project<-rbind(centers_project,data.frame(PC1=NA,PC2=NA,PC3=NA))
 }
-centers_unknown<-rbind(centers_unknown,data.frame(PC1=NA,PC2=NA,PC3=NA))
-row.names(centers_unknown)<-c(clusters_unknown,c(clusters_exclude,1))
-centers_unknown<-as.data.frame(centers_unknown)
+centers_project<-rbind(centers_project,data.frame(PC1=NA,PC2=NA,PC3=NA))
+row.names(centers_project)<-c(clusters_unknown,c(clusters_exclude,1))
+centers_project<-as.data.frame(centers_project)
+print(centers_project)
 
-centers_unknown$dist_AMR<-NA
-centers_unknown$dist_AFR<-NA
-centers_unknown$dist_EAS<-NA
-centers_unknown$dist_EUR<-NA
-centers_unknown$dist_SAS<-NA
-for(i in 1:(nrow(centers_unknown)-(1+length(clusters_exclude)))) {
-	centers_unknown$dist_AMR[i]<-sqrt(sum((centers_unknown[i,1:3]-centers_1kg["AMR",1:3])^2))
-	centers_unknown$dist_AFR[i]<-sqrt(sum((centers_unknown[i,1:3]-centers_1kg["AFR",1:3])^2))
-	centers_unknown$dist_EAS[i]<-sqrt(sum((centers_unknown[i,1:3]-centers_1kg["EAS",1:3])^2))
-	centers_unknown$dist_EUR[i]<-sqrt(sum((centers_unknown[i,1:3]-centers_1kg["EUR",1:3])^2))
-	centers_unknown$dist_SAS[i]<-sqrt(sum((centers_unknown[i,1:3]-centers_1kg["SAS",1:3])^2))
+cat("\ncalculate ditances from project centers to 1kg centers\n")
+centers_project$dist_AMR<-NA
+centers_project$dist_AFR<-NA
+centers_project$dist_EAS<-NA
+centers_project$dist_EUR<-NA
+centers_project$dist_SAS<-NA
+for(i in 1:(nrow(centers_project)-(1+length(clusters_exclude)))) {
+	centers_project$dist_AMR[i]<-sqrt(sum((centers_project[i,1:3]-centers_1kg["AMR",1:3])^2))
+	centers_project$dist_AFR[i]<-sqrt(sum((centers_project[i,1:3]-centers_1kg["AFR",1:3])^2))
+	centers_project$dist_EAS[i]<-sqrt(sum((centers_project[i,1:3]-centers_1kg["EAS",1:3])^2))
+	centers_project$dist_EUR[i]<-sqrt(sum((centers_project[i,1:3]-centers_1kg["EUR",1:3])^2))
+	centers_project$dist_SAS[i]<-sqrt(sum((centers_project[i,1:3]-centers_1kg["SAS",1:3])^2))
 }
-
-centers_unknown$closest1<-NA
-centers_unknown$closest2<-NA
-centers_unknown$ratio<-NA
-for(i in 1:(nrow(centers_unknown)-(1+length(clusters_exclude)))) {
-	s<-sort(centers_unknown[i,c("dist_AMR","dist_AFR","dist_EAS","dist_EUR","dist_SAS")])
-	centers_unknown$closest1[i]<-gsub("dist_","",names(s)[1])
-	centers_unknown$closest2[i]<-gsub("dist_","",names(s)[2])
-	centers_unknown$ratio[i]<-as.numeric(s[2])/as.numeric(s[1])
+centers_project$closest1<-NA
+centers_project$closest2<-NA
+centers_project$ratio<-NA
+for(i in 1:(nrow(centers_project)-(1+length(clusters_exclude)))) {
+	s<-sort(centers_project[i,c("dist_AMR","dist_AFR","dist_EAS","dist_EUR","dist_SAS")])
+	centers_project$closest1[i]<-gsub("dist_","",names(s)[1])
+	centers_project$closest2[i]<-gsub("dist_","",names(s)[2])
+	centers_project$ratio[i]<-as.numeric(s[2])/as.numeric(s[1])
 }
-# centers_unknown$ASSIGNED[! as.integer(row.names(centers_unknown)) %in% c(clusters_exclude,1) & centers_unknown$ratio >= 1.5]<-centers_unknown$closest1[! as.integer(row.names(centers_unknown)) %in% c(clusters_exclude,1) & centers_unknown$ratio >= 1.5]
-# centers_unknown$ASSIGNED[! as.integer(row.names(centers_unknown)) %in% c(clusters_exclude,1) & centers_unknown$ratio < 1.5]<-centers_unknown$closest1[! as.integer(row.names(centers_unknown)) %in% c(clusters_exclude,1) & centers_unknown$ratio < 1.5]
+print(centers_project)
 
-bd<-as.data.frame.matrix(table(dat[,c("CLUSTER","GROUP")]))
-bd<-cbind(bd,as.data.frame.matrix(table(dat[,c("CLUSTER",args$sr_race)])))
-bd$cluster<-as.integer(row.names(bd))
-centers_unknown$ASSIGNED<-"OUTLIERS"
-centers_unknown$cluster<-as.integer(row.names(centers_unknown))
-centers_unknown<-merge(bd,centers_unknown,all=T)
-for(i in 1:nrow(centers_unknown)) {
-	if(! is.na(centers_unknown$ratio[i])) {
-		if(centers_unknown$ratio[i] >= 1.5) {
-			centers_unknown$ASSIGNED[i]<-centers_unknown$closest1[i]
+cat("\ngenerate cluster table and add centers\n")
+cluster_table<-as.data.frame.matrix(table(pheno[,c("CLUSTER","GROUP")]))
+cluster_table<-cbind(cluster_table,as.data.frame.matrix(table(pheno[,c("CLUSTER",args$sr_race)])))
+cluster_table$cluster<-as.integer(row.names(cluster_table))
+centers_project$ASSIGNED<-"OUTLIERS"
+centers_project$cluster<-as.integer(row.names(centers_project))
+cluster_table<-merge(cluster_table,centers_project,all=T)
+print(cluster_table)
+
+cat("\nassign each cluster to nearest 1kg super population\n")
+for(i in 1:nrow(cluster_table)) {
+	print(cluster_table[i,])
+	if(! is.na(cluster_table$ratio[i])) {
+		if(cluster_table$ratio[i] >= 1.5) {
+			cat("  ",paste0("assigning closest1 to cluster ",cluster_table$cluster[i]),"\n")
+			cluster_table$ASSIGNED[i]<-cluster_table$closest1[i]
 		} else {
-			c<-centers_unknown[i,c("AMR","AFR","EAS","EUR","SAS")]
+			cat("  ",paste0("recalculating super population membership based on self report for cluster ",cluster_table$cluster[i]),"\n")
+			c<-cluster_table[i,c("AMR","AFR","EAS","EUR","SAS")]
 			if(args$afr_codes != "") {
 				for(afr_code in unlist(strsplit(args$afr_codes,split=","))) {
-					c$AFR <- c$AFR + centers_unknown[i,paste0("sr_",afr_code)]
+					c$AFR <- c$AFR + cluster_table[i,paste0("sr_",afr_code)]
 				}
 			}
 			if(args$amr_codes != "") {
 				for(amr_code in unlist(strsplit(args$amr_codes,split=","))) {
-					c$AMR <- c$AMR + centers_unknown[i,paste0("sr_",amr_code)]
+					c$AMR <- c$AMR + cluster_table[i,paste0("sr_",amr_code)]
 				}
 			}
 			if(args$eur_codes != "") {
 				for(eur_code in unlist(strsplit(args$eur_codes,split=","))) {
-					c$EUR <- c$EUR + centers_unknown[i,paste0("sr_",eur_code)]
+					c$EUR <- c$EUR + cluster_table[i,paste0("sr_",eur_code)]
 				}
 			}
 			if(args$eas_codes != "") {
 				for(eas_code in unlist(strsplit(args$eas_codes,split=","))) {
-					c$EAS <- c$EAS + centers_unknown[i,paste0("sr_",eas_code)]
+					c$EAS <- c$EAS + cluster_table[i,paste0("sr_",eas_code)]
 				}
 			}
 			if(args$sas_codes != "") {
 				for(sas_code in unlist(strsplit(args$sas_codes,split=","))) {
-					c$SAS <- c$SAS + centers_unknown[i,paste0("sr_",sas_code)]
+					c$SAS <- c$SAS + cluster_table[i,paste0("sr_",sas_code)]
 				}
 			}
 			c<-sort(c,decreasing=TRUE)
 			if(c[1] == 0) {
-				centers_unknown$ASSIGNED[i]<-centers_unknown$closest1[i]
+				cat("  ",paste0("assigning closest1 to cluster ",cluster_table$cluster[i]),"\n")
+				cluster_table$ASSIGNED[i]<-cluster_table$closest1[i]
 			} else {
-				centers_unknown$ASSIGNED[i]<-names(c)[1]
+				cat("  ",paste0("assigning recalculated super population to cluster ",cluster_table$cluster[i]),"\n")
+				cluster_table$ASSIGNED[i]<-names(c)[1]
 			}
 		}
 	}
 }
 sink(file=args$xtabs)
-print(centers_unknown)
+print(cluster_table)
 sink()
 
-centers_unknown_included<-centers_unknown[! centers_unknown$cluster %in% c(clusters_exclude,1),]
+cat("\nplotting cluster centers\n")
+cluster_table_included<-cluster_table[! cluster_table$cluster %in% c(clusters_exclude,1),]
 pdf(args$plots_centers,width=7, height=7)
 for(i in seq(1,2)) {
-	p<-ggplot(dat, aes(dat[,paste("PC",i,sep="")],dat[,paste("PC",i+1,sep="")])) +
+	p<-ggplot(pheno, aes(pheno[,paste("PC",i,sep="")],pheno[,paste("PC",i+1,sep="")])) +
 		geom_point(aes(color=factor(CLUSTER),shape=factor(GROUP))) +
-		geom_point(dat=centers_1kg, aes(centers_1kg[,paste("PC",i,sep="")],centers_1kg[,paste("PC",i+1,sep="")], shape=row.names(centers_1kg)), size=4, colour="black") +
-		geom_text(dat=centers_unknown_included, aes(centers_unknown_included[,paste("PC",i,sep="")],centers_unknown_included[,paste("PC",i+1,sep="")], label=cluster), colour="black") +
+		geom_point(data=centers_1kg, aes(centers_1kg[,paste("PC",i,sep="")],centers_1kg[,paste("PC",i+1,sep="")], shape=row.names(centers_1kg)), size=4, colour="black") +
+		geom_text(data=cluster_table_included, aes(cluster_table_included[,paste("PC",i,sep="")],cluster_table_included[,paste("PC",i+1,sep="")], label=cluster), colour="black") +
 		labs(x=paste("PC",i,sep=""),y=paste("PC",i+1,sep=""),shape="COHORT",colour="CLUSTER") +
 		theme_bw() +
 		guides(col = guide_legend(override.aes = list(shape = 15, size = 10))) +
@@ -194,37 +213,32 @@ for(i in seq(1,2)) {
 }
 dev.off()
 
+cat("\nexporting cluster groups table\n")
 a<-data.frame(ASSIGNED_COHORT=c("AFR","AMR","EAS","EUR","SAS"),stringsAsFactors=F)
 a$CLUSTERS<-NA
-for(i in 1:nrow(centers_unknown)) {
-	if(centers_unknown$ASSIGNED[i] != "OUTLIERS") {
-		if(is.na(a$CLUSTERS[a$ASSIGNED_COHORT == centers_unknown$ASSIGNED[i]])) {
-			a$CLUSTERS[a$ASSIGNED_COHORT == centers_unknown$ASSIGNED[i]]<-row.names(centers_unknown)[i]
+for(i in 1:nrow(cluster_table)) {
+	if(cluster_table$ASSIGNED[i] != "OUTLIERS") {
+		if(is.na(a$CLUSTERS[a$ASSIGNED_COHORT == cluster_table$ASSIGNED[i]])) {
+			a$CLUSTERS[a$ASSIGNED_COHORT == cluster_table$ASSIGNED[i]]<-row.names(cluster_table)[i]
 		} else {
-			a$CLUSTERS[a$ASSIGNED_COHORT == centers_unknown$ASSIGNED[i]]<-paste(a$CLUSTERS[a$ASSIGNED_COHORT == centers_unknown$ASSIGNED[i]],row.names(centers_unknown)[i],sep=",")
+			a$CLUSTERS[a$ASSIGNED_COHORT == cluster_table$ASSIGNED[i]]<-paste(a$CLUSTERS[a$ASSIGNED_COHORT == cluster_table$ASSIGNED[i]],row.names(cluster_table)[i],sep=",")
 		}
 	}
 }
 write.table(a,args$cluster_groups,row.names=F,col.names=F,sep="\t",append=F,quote=F)
 
-dat$ASSIGNED<-"OUTLIERS"
+cat("\nassigning each sample to a 1kg super population based on cluster membership\n")
+pheno$ASSIGNED<-"OUTLIERS"
 for(i in 1:nrow(a)) {
-	dat$ASSIGNED[dat$CLUSTER %in% as.integer(unlist(strsplit(as.character(a$CLUSTERS[i]),split=",")))]<-a$ASSIGNED_COHORT[i]
+	pheno$ASSIGNED[pheno$CLUSTER %in% as.integer(unlist(strsplit(as.character(a$CLUSTERS[i]),split=",")))]<-a$ASSIGNED_COHORT[i]
 }
+write.table(pheno[which(pheno$GROUP == args$project_id),c("IID","ASSIGNED")],args$ancestry_inferred,col.names=F,row.names=F,quote=F,append=F,sep="\t")
 
-### WRITE ASSIGMENTS TO FILES
-write.table(dat[which(dat$GROUP == args$project_id),c("IID","ASSIGNED")],args$ancestry_inferred,col.names=F,row.names=F,quote=F,append=F,sep="\t")
-
-gg_color_hue <- function(n) {
-  hues = seq(15, 375, length=n+1)
-  hcl(h=hues, l=65, c=100)[1:n]
-}
-color<-gg_color_hue(max(dat$CLUSTER))
-
-dat<-dat[which(dat$GROUP == args$project_id & dat$ASSIGNED != "OUTLIERS"),]
+cat("\nplotting clusters without 1kg\n")
+pheno<-pheno[which(pheno$GROUP == args$project_id & pheno$ASSIGNED != "OUTLIERS"),]
 pdf(args$cluster_plots_no1kg,width=7, height=7)
 for(i in seq(1,9)) {
-	p<-ggplot(dat, aes(dat[,paste("PC",i,sep="")],dat[,paste("PC",i+1,sep="")])) +
+	p<-ggplot(pheno, aes(pheno[,paste("PC",i,sep="")],pheno[,paste("PC",i+1,sep="")])) +
 		geom_point(aes(color=factor(CLUSTER),shape=factor(ASSIGNED))) +
 		labs(x=paste("PC",i,sep=""),y=paste("PC",i+1,sep=""),shape="COHORT",colour="CLUSTER") +
 		theme_bw() +
