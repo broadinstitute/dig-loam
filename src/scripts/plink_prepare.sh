6a9c946d06109f99bfa0f44ed4e3469a35893448
ryank <ryank@dig-ae-dev-01.broadinstitute.org>
2019-09-14 01:56:36 -0400

    added --seed 1 to all plink commands for reproducibility

--- src/scripts/plink_prepare.sh
+++ src/scripts/plink_prepare.sh
@@ -8,14 +8,14 @@ dupRemove=$5
 mem=$6
 
 awk '{print $1, $2, $2, $2}' ${rawBase}.fam > ${preparedBase}.nofid.updateids
-$plink --bfile $rawBase --allow-no-sex --update-ids ${preparedBase}.nofid.updateids --keep-allele-order --make-bed --out ${preparedBase}.nofid --memory $mem
+$plink --bfile $rawBase --allow-no-sex --update-ids ${preparedBase}.nofid.updateids --keep-allele-order --make-bed --out ${preparedBase}.nofid --memory $mem --seed 1
 rm ${preparedBase}.nofid.updateids
 awk '{print $1, $2, "0", "0"}' ${preparedBase}.nofid.fam > ${preparedBase}.nofid.updateparents 
-$plink --bfile ${preparedBase}.nofid --allow-no-sex --update-parents ${preparedBase}.nofid.updateparents --keep-allele-order --make-bed --out ${preparedBase}.nofid.noparents --memory $mem
+$plink --bfile ${preparedBase}.nofid --allow-no-sex --update-parents ${preparedBase}.nofid.updateparents --keep-allele-order --make-bed --out ${preparedBase}.nofid.noparents --memory $mem --seed 1
 awk '{print $1, $2, "0"}' ${preparedBase}.nofid.noparents.fam > ${preparedBase}.nofid.noparents.updatesex
-$plink --bfile ${preparedBase}.nofid.noparents --allow-no-sex --update-sex ${preparedBase}.nofid.noparents.updatesex --keep-allele-order --make-bed --out ${preparedBase}.nofid.noparents.nosex --memory $mem
+$plink --bfile ${preparedBase}.nofid.noparents --allow-no-sex --update-sex ${preparedBase}.nofid.noparents.updatesex --keep-allele-order --make-bed --out ${preparedBase}.nofid.noparents.nosex --memory $mem --seed 1
 rm ${preparedBase}.nofid.noparents.updatesex
 awk '{print $1, $2, "-9"}' ${preparedBase}.nofid.noparents.nosex.fam > ${preparedBase}.nofid.noparents.nosex.updatepheno
-$plink --bfile ${preparedBase}.nofid.noparents.nosex --allow-no-sex --pheno ${preparedBase}.nofid.noparents.nosex.updatepheno --keep-allele-order --make-bed --out ${preparedBase}.nofid.noparents.nosex.nopheno --memory $mem
-$plink --bfile ${preparedBase}.nofid.noparents.nosex.nopheno --remove $imissRemove --exclude $dupRemove --merge-x no-fail --allow-no-sex --output-chr MT --make-bed --out $preparedBase --memory $mem
+$plink --bfile ${preparedBase}.nofid.noparents.nosex --allow-no-sex --pheno ${preparedBase}.nofid.noparents.nosex.updatepheno --keep-allele-order --make-bed --out ${preparedBase}.nofid.noparents.nosex.nopheno --memory $mem --seed 1
+$plink --bfile ${preparedBase}.nofid.noparents.nosex.nopheno --remove $imissRemove --exclude $dupRemove --merge-x no-fail --allow-no-sex --output-chr MT --make-bed --out $preparedBase --memory $mem --seed 1
 rm ${preparedBase}.nofid.*
--- src/scripts/plink_to_vcf_no_half_calls.sh
+++ src/scripts/plink_to_vcf_no_half_calls.sh
@@ -7,6 +7,6 @@ refBase=$4
 mem=$5
 vcf=$6
 
-$plink --bfile $base --allow-no-sex --output-chr MT --recode vcf-iid bgz --real-ref-alleles --a2-allele $forceA2 --out ${refBase}.temp --memory $mem
-$plink --vcf ${refBase}.temp.vcf.gz --allow-no-sex --output-chr MT --vcf-half-call haploid --recode vcf-iid bgz --real-ref-alleles --a2-allele $forceA2 --out $refBase --memory $mem
+$plink --bfile $base --allow-no-sex --output-chr MT --recode vcf-iid bgz --real-ref-alleles --a2-allele $forceA2 --out ${refBase}.temp --memory $mem --seed 1
+$plink --vcf ${refBase}.temp.vcf.gz --allow-no-sex --output-chr MT --vcf-half-call haploid --recode vcf-iid bgz --real-ref-alleles --a2-allele $forceA2 --out $refBase --memory $mem --seed 1
 rm ${refBase}.temp.*
