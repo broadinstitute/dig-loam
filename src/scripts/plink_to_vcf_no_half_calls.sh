de946bf795133bd9650fdbf918a07a3d72dd1309
ryank <ryank@dig-ae-dev-01.broadinstitute.org>
2019-09-14 01:49:24 -0400

    added --seed 1 to all plink commands for reproducibility

--- src/scripts/plink_to_vcf_no_half_calls.sh
+++ src/scripts/plink_to_vcf_no_half_calls.sh
@@ -7,5 +7,5 @@ refBase=$4
 mem=$5
 vcf=$6
 
-$plink --bfile $base --allow-no-sex --output-chr MT --recode vcf-iid bgz --real-ref-alleles --a2-allele $forceA2 --out $refBase --memory $mem
-$plink --vcf $vcf --allow-no-sex --output-chr MT --vcf-half-call haploid --recode vcf-iid bgz --real-ref-alleles --a2-allele $forceA2 --out $refBase --memory $mem
+$plink --bfile $base --allow-no-sex --output-chr MT --recode vcf-iid bgz --real-ref-alleles --a2-allele $forceA2 --out $refBase --memory $mem --seed 1
+$plink --vcf $vcf --allow-no-sex --output-chr MT --vcf-half-call haploid --recode vcf-iid bgz --real-ref-alleles --a2-allele $forceA2 --out $refBase --memory $mem --seed 1
