0ede1a7bbd53503d523fbbb3cd90995a4885b239
ryank <ryank@dig-ae-dev-01.broadinstitute.org>
2020-02-26 02:51:25 -0500

    added hail_filter_schema_pheno_variants.py to produce pheno level filter file as necessary

--- src/scripts/hail_filter_schema_pheno_variants.py
+++ src/scripts/hail_filter_schema_pheno_variants.py
@@ -32,16 +32,16 @@ def main(args=None):
 	print("key rows by locus, alleles and rsid")
 	ht = ht.key_by('locus','alleles','rsid')
 
-	if args.variants_remove is not None:
-		print("flag variants for removal that failed previous qc steps")
-		ht = ht.annotate(ls_previous_exclude = 0)
-		for variant_file in args.variants_remove.split(","):
-			try:
-				tbl = hl.import_table(variant_file, no_header=True, types={'f0': 'locus<' + args.reference_genome + '>', 'f1': 'array<str>', 'f2': 'str'}).key_by('f0', 'f1', 'f2')
-			except:
-				print("skipping empty file " + variant_file)
-			else:
-				ht = ht.annotate(ls_previous_exclude = hl.cond(hl.is_defined(tbl[ht.key]), 1, ht.ls_previous_exclude))
+	print("annotate hail table with pheno stats")
+	tbl = hl.read_table(args.pheno_stats_in, )
+	tbl = tbl.key_by('locus','alleles','rsid')
+	tbl = tbl.select(*['variant_qc'])
+	ht = ht.annotate(**{'variant_qc': ht['variant_qc'].annotate(**tbl[ht.key]['variant_qc'])})
+
+	print("annotate hail table with schema filters")
+	tbl = hl.read_table(args.schema_filters_in, )
+	tbl = tbl.key_by('locus','alleles','rsid')
+	ht = ht.join(tbl)
 
 	print("key rows by locus and alleles")
 	ht = ht.key_by('locus','alleles')
@@ -111,10 +111,8 @@ def main(args=None):
 		tbl = tbl.key_by('Uploaded_variation')
 		ht = ht.annotate(annotation = tbl[ht.rsid])
 
-	start_time = time.time()
+	print("write checkpoint hail table to disk")
 	ht = ht.checkpoint(args.ht_checkpoint, overwrite=True)
-	elapsed_time = time.time() - start_time
-	print(time.strftime("write checkpoint hail table to disk - %H:%M:%S", time.gmtime(elapsed_time)))
 
 	cohort_stats = {}
 	if args.cohort_stats_in:
@@ -124,73 +122,49 @@ def main(args=None):
 	exclude_any_fields = []
 	fields_out = ['rsid','annotation']
 
-	# initialize max_cohort_maf
-	if len(cohorts) > 1:
-		start_time = time.time()
-		ht = ht.annotate(max_cohort_maf = 0)
-		fields_out = fields_out + ['max_cohort_maf']
-		elapsed_time = time.time() - start_time
-		print(time.strftime("initialize max_cohort_maf to 0 - %H:%M:%S", time.gmtime(elapsed_time)))
-
-	# read in each cohort table, calculate max_cohort_maf, and add cohort_filters and knockout_filters
+	# read in each cohort table and add cohort_filters and knockout_filters
 	knockout_fields = []
 	if len(cohorts) > 0:
 		for cohort in cohorts:
-			start_time = time.time()
+			print("annotate rows with cohort level variant qc in " + cohort)
 			cohort_ht = hl.read_table(cohort_stats[cohort])
 			ht = ht.annotate(**{'variant_qc_' + cohort: cohort_ht[ht.key]['variant_qc']})
 			if cohort in cohort_filters.keys():
-				start_time = time.time()
-				cohort_ht = hail_utils.ht_add_filters(cohort_ht, cohort_filters[cohort], 'ls_filters_' + cohort)
-				ht = ht.annotate(**{'ls_filters_' + cohort: cohort_ht[ht.key]['ls_filters_' + cohort]})
-				fields_out = fields_out + ['ls_filters_' + cohort]
-				exclude_any_fields = exclude_any_fields + ['ls_filters_' + cohort + '.exclude']
-				elapsed_time = time.time() - start_time
-				print(time.strftime("add cohort filters for cohort " + cohort + " - %H:%M:%S", time.gmtime(elapsed_time)))
+				print("add cohort filters for cohort " + cohort)
+				cohort_ht = hail_utils.ht_add_filters(cohort_ht, cohort_filters[cohort], 'ls_pheno_filters_' + cohort)
+				ht = ht.annotate(**{'ls_pheno_filters_' + cohort: cohort_ht[ht.key]['ls_pheno_filters_' + cohort]})
+				fields_out = fields_out + ['ls_pheno_filters_' + cohort]
+				exclude_any_fields = exclude_any_fields + ['ls_pheno_filters_' + cohort + '.exclude']
 			if cohort in knockout_filters.keys():
-				start_time = time.time()
+				print("add knockout filters for cohort " + cohort)
 				cohort_ht = hail_utils.ht_add_filters(cohort_ht, knockout_filters[cohort], 'ls_knockouts_' + cohort)
 				ht = ht.annotate(**{'ls_knockouts_' + cohort: cohort_ht[ht.key]['ls_knockouts_' + cohort]})
 				fields_out = fields_out + ['ls_knockouts_' + cohort]
 				knockout_fields = knockout_fields + ['ls_knockouts_' + cohort + '.exclude']
-				elapsed_time = time.time() - start_time
-				print(time.strftime("add knockout filters for cohort " + cohort + " - %H:%M:%S", time.gmtime(elapsed_time)))
-			start_time = time.time()
-			elapsed_time = time.time() - start_time
-			print(time.strftime("annotate rows with cohort level variant qc in " + cohort + " - %H:%M:%S", time.gmtime(elapsed_time)))
-			if len(cohorts) > 1:
-				start_time = time.time()
-				ht = ht.annotate(max_cohort_maf = hl.cond( ht.max_cohort_maf < ht['variant_qc_' + cohort].MAF, ht['variant_qc_' + cohort].MAF, ht.max_cohort_maf))
-				elapsed_time = time.time() - start_time
-				print(time.strftime("annotate rows with updated max_cohort_maf for cohort " + cohort + " - %H:%M:%S", time.gmtime(elapsed_time)))
 
 	# add filters for full variant stats
 	if len(standard_filters) > 0:
-		exclude_any_fields = exclude_any_fields + ['ls_filters.exclude']
-		ht = hail_utils.ht_add_filters(ht, standard_filters, 'ls_filters')
-		fields_out = fields_out + ['ls_filters']
+		exclude_any_fields = exclude_any_fields + ['ls_pheno_filters.exclude']
+		ht = hail_utils.ht_add_filters(ht, standard_filters, 'ls_pheno_filters')
+		fields_out = fields_out + ['ls_pheno_filters']
 
 	mask_fields = []
 	if len(masks) > 0:
 		mask_ids = [x[0] for x in masks]
 		for m in set(mask_ids):
-			start_time = time.time()
+			print("add mask " + m + " to filters")
 			masks_row = [x for x in masks if x[0] == m]
 			ht = hail_utils.ht_add_filters(ht, [[x[1],x[2],x[3]] for x in masks_row], 'ls_mask_' + m)
 			mask_fields = mask_fields + ['ls_mask_' + m + '.exclude']
 			fields_out = fields_out + ['ls_mask_' + m]
-			elapsed_time = time.time() - start_time
-			print(time.strftime("add mask " + m + " to filters - %H:%M:%S", time.gmtime(elapsed_time)))
 
-	fields_out = fields_out + ['ls_previous_exclude','ls_global_exclude']
-	ht = ht.annotate(ls_global_exclude = 0)
-	ht = ht.annotate(ls_global_exclude = hl.cond(ht.ls_previous_exclude == 1, 1, ht.ls_global_exclude))
+	fields_out = fields_out + ['ls_global_exclude', 'ls_pheno_global_exclude']
+	ht = ht.annotate(ls_pheno_global_exclude = 0)
+	ht = ht.annotate(ls_pheno_global_exclude = hl.cond(ht.ls_global_exclude == 1, 1, ht.ls_pheno_global_exclude))
 	for f in exclude_any_fields:
-		start_time = time.time()
+		print("update global exclusion column based on " + f)
 		ls_struct, ls_field = f.split(".")
-		ht = ht.annotate(ls_global_exclude = hl.cond(ht[ls_struct][ls_field] == 1, 1, ht.ls_global_exclude))
-		elapsed_time = time.time() - start_time
-		print(time.strftime("update global exclusion column based on " + f + " - %H:%M:%S", time.gmtime(elapsed_time)))
+		ht = ht.annotate(ls_pheno_global_exclude = hl.cond(ht[ls_struct][ls_field] == 1, 1, ht.ls_pheno_global_exclude))
 
 	# select fields to keep from hail table and write to file
 	ht = ht.select(*fields_out)
@@ -216,7 +190,6 @@ if __name__ == "__main__":
 	parser.add_argument('--hail-utils', help='a path to a python file containing hail functions')
 	parser.add_argument('--reference-genome', choices=['GRCh37','GRCh38'], default='GRCh37', help='a reference genome build code')
 	parser.add_argument('--annotation', help="a hail table containing annotations from vep")
-	parser.add_argument('--variants-remove', help="a comma separated list of files containing variants to remove")
 	parser.add_argument('--filters', help='filter id, column name, expression; exclude variants satisfying this expression')
 	parser.add_argument('--cohort-filters', help='cohort id, filter id, column name, expression; exclude variants satisfying this expression')
 	parser.add_argument('--knockout-filters', help='cohort id, filter id, column name, expression; exclude variants satisfying this expression')
@@ -227,6 +200,8 @@ if __name__ == "__main__":
 	requiredArgs = parser.add_argument_group('required arguments')
 	requiredArgs.add_argument('--log', help='a hail log filename', required=True)
 	requiredArgs.add_argument('--full-stats-in', help='a hail table with variant stats on full sample set', required=True)
+	requiredArgs.add_argument('--pheno-stats-in', help='a hail table with variant stats on sample set non-missing for phenotype', required=True)
+	requiredArgs.add_argument('--schema-filters-in', help='a hail table with schema level variant filters on full sample set', required=True)
 	requiredArgs.add_argument('--ht-checkpoint', help='a checkpoint filename', required=True)
 	requiredArgs.add_argument('--variant-filters-ht-out', help='a filename for variant filters hail table', required=True)
 	args = parser.parse_args()
