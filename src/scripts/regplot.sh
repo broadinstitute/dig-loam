a52002bd34b7856bc14f180999def2826967bea1
ryank <ryank@dig-ae-dev-01.broadinstitute.org>
2020-06-21 12:36:28 -0400

    fixed bugs in regplot.sh occuring when refsnp is not in source

--- src/scripts/regplot.sh
+++ src/scripts/regplot.sh
@@ -15,6 +15,16 @@ exitCode=0
 
 echo "$nSig significant variants found" > ${out}.log
 
+sourcePath=""
+if [[ "$source" =~ 2014$ ]]; then
+	sourcePath="/usr/local/src/locuszoom/data/1000G/genotypes/2014-10-14"
+elif [[ "$source" =~ 2017$ ]]; then
+	sourcePath="/usr/local/src/locuszoom/data/1000G/genotypes/2017-04-10"
+else
+	echo "source path not available for ${source}"
+	exit 1
+fi
+
 if [ "$nSig" -ge "1" ]; then
 	prefixArray=()
 	dataArray=()
@@ -31,25 +41,70 @@ if [ "$nSig" -ge "1" ]; then
 		prefixArray+=("$prefix")
 		data=${prefix}.results.tsv
 		dataArray+=("$data")
-		dir=${prefix}_chr${regionChr}_${regionStart}-${regionEnd}
-		dirArray+=("$dir")
-		pdf=${dir}/chr${regionChr}_${regionStart}-${regionEnd}.pdf
-		pdfArray+=("$pdf")
 		log=${prefix}.log
 		logArray+=("$log")
 		(echo -e "id\tpval"; $tabix $results ${regionChr}:${regionStart}-${regionEnd} | awk -v pcol=$pcol '{if($pcol != "NA" && $pcol != "NaN") { if(substr($3 ,0, 2) != "rs") { print "chr"$1":"$2"\t"$pcol } else print $3"\t"$pcol} }') > $data
-		echo $line
-		echo $regionChr
-		echo $regionStart
-		echo $regionEnd
-		echo $regionVar
-		echo $data
-		options="--metal $data --chr $regionChr --start $regionStart --end $regionEnd --markercol id --pvalcol pval --no-date --prefix $prefix --cache None --build $build --pop $pop --source $source"
-		if [ "$regionChr" -ge "23" ]; then
+		ldRefVars=`sed '1d' $data | sort -g -k2,2 | awk '{print $1}' | head -10 | tr '\n' ' '`
+		refVar=""
+		refVarId=""
+		for id in $ldRefVars; do
+			chr=`echo $id | awk -F':' '{print $1}' | sed 's/chr//g'`
+			pos=`echo $id | awk -F':' '{print $2}'`
+			n=`awk -v c=$chr -v p=$pos '{if($1 == c && $4 == p) print $0}' ${sourcePath}/${pop}/chr${chr}.bim | wc -l`
+			if [ $n == 1 ]; then
+				refVar=$id
+				m=`tabix $results ${chr}:${pos}-${pos} | wc -l`
+				if [ $m == 1 ]; then
+					v=`tabix $results ${chr}:${pos}-${pos} | awk '{print $3}'`
+					if [[ "$v" =~ ^rs ]]; then
+						refVarId=$v
+					else
+						refVarId=$refVar
+					fi
+				else
+					l=`tabix $results ${chr}:${pos}-${pos} | grep -w "${regionVar}" | wc -l`
+					if [ $l == 1]; then
+						refVarId=$regionVar
+					else
+						v=`tabix $results ${chr}:${pos}-${pos} | head -1 | awk '{print $3}'`
+						if [[ "$v" =~ ^rs ]]; then
+							refVarId=$v
+						else
+							refVarId=$refVar
+						fi
+					fi
+				fi
+				break
+			fi
+		done
+		echo -e "sig region: ${line}"
+		echo -e "chr: ${regionChr}"
+		echo -e "start: ${regionStart}"
+		echo -e "end: ${regionEnd}"
+		echo -e "variant: ${regionVar}"
+		echo -e "formatted input data: ${data}"
+		echo -e "sorted possible ld ref variants: ${ldRefVars}"
+		echo -e "ld ref var: ${refVarId}"
+		
+		options="--metal $data --chr $regionChr --start $regionStart --end $regionEnd --markercol id --pvalcol pval --no-date --prefix $prefix --cache None --build $build --pop $pop --source $source --no-cleanup"
+		if [[ "$regionChr" -ge "23" || "${refVarId}" == "" ]]; then
 			options="$options --no-ld"
+		else
+			options="$options --refsnp ${refVarId}"
 		fi
+
 		echo "Locuszoom arguments: $options" >> ${out}.log
 		$locuszoom $options > $log
+
+		if [[ "$refVarId" =~ ^rs ]]; then
+			dir=`echo "${prefix}_${refVarId}" | sed 's/:/_/g'`
+		else
+			dir=`echo "${prefix}_chr${regionChr}_${regionStart}-${regionEnd}" | sed 's/:/_/g'`
+		fi
+		dirArray+=("$dir")
+		pdf=${dir}/chr${regionChr}_${regionStart}-${regionEnd}.pdf
+		pdfArray+=("$pdf")
+
 		exitCode=$?
 	done < $sigregions
 	$ghostscript -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dFirstPage=1 -dLastPage=1 -sOutputFile=${out}.pdf $(IFS=" "; echo "${pdfArray[*]}")
@@ -57,9 +112,9 @@ if [ "$nSig" -ge "1" ]; then
 		exitCode=$?
 	fi
 	cat $(IFS=" "; echo "${logArray[*]}") >> ${out}.log
-	#rm $(IFS=" "; echo "${logArray[*]}")
-	#rm $(IFS=" "; echo "${dataArray[*]}")
-	#rm -r $(IFS=" "; echo "${dirArray[*]}")
+	rm $(IFS=" "; echo "${logArray[*]}")
+	rm $(IFS=" "; echo "${dataArray[*]}")
+	rm -r $(IFS=" "; echo "${dirArray[*]}")
 else
 	touch ${out}.pdf
 fi
