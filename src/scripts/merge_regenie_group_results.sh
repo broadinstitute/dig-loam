bfea2db0b6b206c140d1106cf7f554a2f3ef8581
ryank <ryank@dig-ae-dev-02.broadinstitute.org>
2021-07-14 17:10:50 -0400

    fixed bug in merge_regenie_group_results.sh leading to missing header

--- src/scripts/merge_regenie_group_results.sh
+++ src/scripts/merge_regenie_group_results.sh
@@ -51,9 +51,9 @@ results1=`echo $results | sed "s/___CHR___/${chrs1}/g"`; \
 
 IFS=',' read -r -a array <<< "$chrs"
 
-(zcat $results1 | head -2 | tail -1; \
+(zcat $results1 | head -1; \
 (for chr in "${array[@]}"; do \
 	resultsFile=`echo $results | sed "s/___CHR___/${chr}/g"`; \
-	zcat $resultsFile | sed '1,2d'; \
+	zcat $resultsFile | sed '1d'; \
 done) | awk '{if($1 == "X") { idx=23 } else { if($1 == "Y") { idx=24 } else { if($1 == "MT") { idx=25 } else { idx=$1 } } } print idx"\t"$0 }' | sort -n -k1,1 -k3,3 | cut -f2- \
 ) | bgzip -c > $out
