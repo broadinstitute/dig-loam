3f7e245abec004b2860d2aed2a83c2e932d893bb
ryank <ryank@dig-ae-dev-02.broadinstitute.org>
2021-08-23 09:10:10 -0400

    fixed small bug from 2 commits ago

--- src/scala/assoc/AssocRegenie.scala
+++ src/scala/assoc/AssocRegenie.scala
@@ -88,7 +88,7 @@ object AssocRegenie extends loamstream.LoamFile {
           --pheno-file ${modelStores((configModel, configSchema, configCohorts, configMeta)).regenie.get.pheno}
           --pheno-name ${configModel.finalPheno}
           --block-size ${projectConfig.regenieBlockSize.get}
-          --min-mac ${projectConfig.regenieMinMAC.get}
+          --min-mac ${projectConfig.regenieSingleMinMAC.get}
           ${btString}
           --chr ${chr}
           --pred ${modelStores((configModel, configSchema, configCohorts, configMeta)).regenie.get.step1.predList}
