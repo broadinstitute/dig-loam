d21220a1c2f421d34b0c79a68ca61aae2aa38daf
ryank <ryank@dig-ae-dev-02.broadinstitute.org>
2022-08-04 13:33:21 -0400

    fixed bugs in annotate_results.sh, hail_filter_schema_pheno_variants.py, and hail_filter_schema_variants.py

--- src/scripts/hail_filter_schema_variants.py
+++ src/scripts/hail_filter_schema_variants.py
@@ -135,9 +135,9 @@ def main(args=None):
 			user_annot_tbl = hl.read_table(user_annot_ht)
 			user_annot_tbl = user_annot_tbl.select(*user_annotation_fields)
 			if user_annot_incl_gene == "true":
-				ht = ht.annotate('file_' + user_annot_id = user_annot_tbl[ht.locus,ht.alleles,ht.annotation.Gene])
+				ht = ht.annotate(**{'file_' + user_annot_id: user_annot_tbl[ht.locus,ht.alleles,ht.annotation.Gene]})
 			else:
-				ht = ht.annotate('file_' + user_annot_id = user_annot_tbl[ht.locus,ht.alleles])
+				ht = ht.annotate(**{'file_' + user_annot_id: user_annot_tbl[ht.locus,ht.alleles]})
 	
 	start_time = time.time()
 	print("write ht.checkpoint1 hail table to temporary directory")
