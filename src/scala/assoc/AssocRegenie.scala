2f1c0c942cf1a5ae72f8a8e5323c882631ce2bc4
ryank <ryank@dig-ae-dev-02.broadinstitute.org>
2023-01-23 10:56:48 -0500

    added function chrNumberToCode to convert chromosome codes from 1-22,X,Y,M to chr1-chr22,chrX,chrY,chrM where GRCh38 codes are needed

--- src/scala/assoc/AssocRegenie.scala
+++ src/scala/assoc/AssocRegenie.scala
@@ -78,7 +78,7 @@ object AssocRegenie extends loamstream.LoamFile {
           --pheno-file ${modelStores((configModel, configSchema, configCohorts, configMeta)).regenie.get.pheno}
           --pheno-names "${phenosAnalyzed}"
           --cli-options "${configTest.cliOpts.get}"
-          --chr ${chr}
+          --chr ${chrNumberToCode(chr, projectConfig.referenceGenome)}
           --pred ${modelStores((configModel, configSchema, configCohorts, configMeta)).regenie.get.step1.predList}
           --out ${modelStores((configModel, configSchema, configCohorts, configMeta)).regenie.get.assocSingle(configTest).chrs(chr).base}"""
           .in(arrayStores(array).bgen.get.data.local.get, arrayStores(array).bgen.get.sample.local.get, modelStores((configModel, configSchema, configCohorts, configMeta)).regenie.get.covars, modelStores((configModel, configSchema, configCohorts, configMeta)).regenie.get.pheno, modelStores((configModel, configSchema, configCohorts, configMeta)).regenie.get.step1.predList)
@@ -302,7 +302,7 @@ object AssocRegenie extends loamstream.LoamFile {
           --pheno-names "${phenosAnalyzed}"
           --cli-options "${configTest.cliOpts.get}"
           --group-stats
-          --chr ${chr}
+          --chr ${chrNumberToCode(chr, projectConfig.referenceGenome)}
           --pred ${modelStores((configModel, configSchema, configCohorts, configMeta)).regenie.get.step1.predList}
           --anno-file ${schemaStores((configSchema, configCohorts)).regenie.get.annotations(mask).local.get}
           --set-list ${schemaStores((configSchema, configCohorts)).regenie.get.setlist.local.get}
